8f2eb62eea2a23f233967f3df5230aeeb8637816
Station Chenqiao <i@cctv3.net>
2022-03-02 20:41:05 +0800

    🆕 新增卡片样式 `useCardStyle`。

--- src/ui.ts
+++ src/ui.ts
@@ -1,4 +1,4 @@
-import {Dimensions, Platform, StatusBar} from 'react-native';
+import { Dimensions, Platform, StatusBar } from "react-native";
 
 /**
  * 判断是否 iPhone 11
@@ -6,10 +6,10 @@ import {Dimensions, Platform, StatusBar} from 'react-native';
  */
 const isiPhone11 = () => {
   const screenHeights = [780, 812, 844, 896, 926];
-  const screen = Dimensions.get('screen');
+  const screen = Dimensions.get("screen");
   return (
-    Platform.OS == 'ios' &&
-    screenHeights.some(it => it == screen.height || it == screen.width)
+    Platform.OS == "ios" &&
+    screenHeights.some((it) => it == screen.height || it == screen.width)
   );
 };
 
@@ -18,7 +18,7 @@ const isiPhone11 = () => {
  * @returns string
  */
 const useRandomColor = () => {
-  return `#${parseInt(Math.random().toString().replace('0.', ''))
+  return `#${parseInt(Math.random().toString().replace("0.", ""))
     .toString(16)
     .substring(0, 6)}`;
 };
@@ -44,9 +44,36 @@ const useHomeIndicatorHeight = () => {
   return isiPhone11() ? 34 : 0;
 };
 
+/**
+ * 卡片样式
+ * @param shadowColor 默认: #000
+ * @param shadowOffset 默认: {width: 0, height: 1}
+ * @param shadowOpacity 默认: 0.1
+ * @param elevation 默认: 4
+ * @returns
+ */
+type Offset = {
+  width: number;
+  height: number;
+};
+const useCardStyle = (
+  shadowColor?: string,
+  shadowOffset?: Offset,
+  shadowOpacity?: number,
+  elevation?: number
+) => {
+  return {
+    shadowColor: shadowColor ?? "#000",
+    shadowOffset: shadowOffset ?? { width: 0, height: 1 },
+    shadowOpacity: shadowOpacity ?? 0.1,
+    elevation: elevation ?? 4,
+  };
+};
+
 export default {
   isiPhone11,
   useRandomColor,
   useStatusBarHeight,
   useHomeIndicatorHeight,
+  useCardStyle,
 };
